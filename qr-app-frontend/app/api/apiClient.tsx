5448046db926eeb79b3dfa47730092734fae03dd
Piyush Gurjar <piyush.g@consultadd.com>
2025-06-06 20:37:18 +0530

    backendUrl change

--- qr-app-frontend/app/api/apiClient.tsx
+++ qr-app-frontend/app/api/apiClient.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',  
-  // baseURL: 'https://qr-app-w9yr.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',  
+  baseURL: 'https://qr-app-w9yr.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
